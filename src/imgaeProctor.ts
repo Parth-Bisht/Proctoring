6b94c1087d1556735c3c51d07495bd49b6354575
Parth Bisht <bishtparth795@gmail.com>
2023-07-31 11:16:10 +0530

    image proctor changes

--- src/imgaeProctor.ts
+++ src/imgaeProctor.ts
@@ -1,6 +1,9 @@
 class ImageDetector {
-  private interval: number = 1000;
-  private videoElement: HTMLVideoElement;
+  private shutterTimer: number = 3000;
+  private videoElement: any;
+  private modal: HTMLDivElement;
+  private timer: HTMLParagraphElement;
+  private box: HTMLDivElement;
   private canvasElement: HTMLCanvasElement;
   private localStorageKey: string = "Capture";
   private intervalId: number | any;
@@ -8,24 +11,27 @@ class ImageDetector {
 
   constructor() {
     this.videoElement = document.createElement("video");
+    this.modal = document.createElement("div");
+    this.box = document.createElement("div");
+    this.timer = document.createElement("p");
     this.canvasElement = document.createElement("canvas");
     this.mediaStream = null;
   }
 
-  startCapture(interval: number, key: string): void {
-    this.interval = interval;
+  startCapture(key: string, shutterTime: number): void {
+    this.shutterTimer = shutterTime;
     this.localStorageKey = key;
+    this.createVideoBox();
     navigator.mediaDevices
-      .getUserMedia({ video: true })
+      .getUserMedia({ video: true, audio: false })
       .then((stream) => {
         this.mediaStream = stream;
-        this.videoElement.srcObject = stream;
-        // this.videoElement.addEventListener("loadedmetadata", () => {
-          this.intervalId = setInterval(
-            () => this.captureImage(),
-            this.interval
-          );
-        // });
+        this.videoElement.srcObject = this.mediaStream;
+        document.body.appendChild(this.modal);
+        this.videoElement.play();
+        this.videoElement.addEventListener("loadedmetadata", () => {
+          this.captureImage();
+        });
       })
       .catch((error) => {
         console.error("Error accessing webcam:", error);
@@ -43,22 +49,65 @@ class ImageDetector {
   }
 
   private captureImage(): void {
+    clearInterval(this.intervalId);
+    this.timer.innerText = this.shutterTimer.toString();
     this.canvasElement.width = this.videoElement.videoWidth;
     this.canvasElement.height = this.videoElement.videoHeight;
+    this.intervalId = setInterval(() => {
+      this.shutterTimer -= 1;
+      this.timer.innerText = this.shutterTimer.toString();
+      if (this.shutterTimer < 0) {
+        this.timer.innerText = "";
+        clearInterval(this.intervalId);
+        const context = this.canvasElement.getContext("2d");
+        context?.drawImage(
+          this.videoElement,
+          0,
+          0,
+          this.canvasElement.width,
+          this.canvasElement.height
+        );
+        const imageDataURL = this.canvasElement.toDataURL("image/jpeg");
+        console.log(imageDataURL);
+        this.modal.style.display = "none";
 
-    const context = this.canvasElement.getContext("2d");
-    context?.drawImage(
-      this.videoElement,
-      0,
-      0,
-      this.canvasElement.width,
-      this.canvasElement.height
-    );
-    const imageDataURL = this.canvasElement.toDataURL("image/jpeg");
-    console.log(imageDataURL);
+        try {
+          localStorage.setItem(this.localStorageKey, imageDataURL);
+          console.log("Image captured and saved in localStorage.");
+          this.stopCapture();
+        } catch (error) {
+          console.log("Error storing the image in localstorage", error);
+          this.stopCapture();
+        }
+      }
+    }, 1000);
+  }
 
-    localStorage.setItem(this.localStorageKey, imageDataURL);
-    console.log("Image captured and saved in localStorage.");
+  private createVideoBox(): void {
+    this.videoElement.width = "320";
+    this.videoElement.height = "240";
+    this.box.style.width = "320px";
+    this.box.style.height = "240px";
+    this.box.style.position = "relative";
+    this.box.appendChild(this.videoElement);
+    this.box.appendChild(this.timer);
+    this.timer.style.position = "absolute";
+    this.timer.style.fontSize = "20px";
+    this.timer.style.top = "13px";
+    this.timer.style.left = "125px";
+    this.timer.style.color = "white";
+    this.timer.style.fontSize = "69px";
+
+    this.modal.style.border = "1px solid";
+    this.modal.style.position = "absolute";
+    this.modal.style.width = "100vw";
+    this.modal.style.height = "100vh";
+    this.modal.style.top = "0";
+    this.modal.style.left = "0";
+    this.modal.style.display = "flex";
+    this.modal.style.justifyContent = "center";
+    this.modal.style.background = "#9494947a";
+    this.modal.appendChild(this.box);
   }
 }
 
